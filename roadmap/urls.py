852b4a5fac8935bc917552e4b52385efa6f361c6
Nimrod A. Abing <nimrod@atlas17.com>
2019-02-02 16:54:05 +0800

    Added api/ base URL to project urls module.
    Load urlpatterns from roadmap.api.urls.

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import include, patterns, url
 
 
 urlpatterns = patterns(
     'roadmap.views',
     url(r'^$', 'dashboard', name='dashboard')
 )
+
+urlpatterns += [
+    url(r'^api/', include('roadmap.api.urls', namespace='api'))
+]
