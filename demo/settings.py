646c62de6d5b384815a6b0e15f360d3c07e2a3d2
Nimrod A. Abing <nimrod@atlas17.com>
2019-02-02 16:36:54 +0800

    Add rest_framework to INSTALLED_APPS

--- demo/settings.py
+++ demo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'roadmap'
+    'roadmap',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
