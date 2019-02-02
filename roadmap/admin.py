3f4fbafc3b389a070085e2d84e9ca4a4ebdb28b7
Nimrod A. Abing <nimrod@atlas17.com>
2019-02-02 16:41:06 +0800

    Hook up project, roadmap, and step models to Admin.

--- roadmap/admin.py
+++ roadmap/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from roadmap.models import Project, Roadmap, RoadmapStep
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'roadmap', 'owner')
+
+
+class RoadmapAdmin(admin.ModelAdmin):
+    list_display = ('name', 'steps_count')
+
+    def steps_count(self, obj):
+        return obj.steps.count()
+    steps_count.short_description = 'Number of Steps'
+
+
+class RoadmapStepAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Roadmap, RoadmapAdmin)
+admin.site.register(RoadmapStep, RoadmapStepAdmin)
